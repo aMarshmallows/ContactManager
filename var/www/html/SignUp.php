b3370af66dc4896aee1ca62ffdb9c8d29cd84b01
JetGrind19 <spab719@gmail.com>
2022-09-07 14:54:19 -0400

    Fixed SignUp

--- var/www/html/SignUp.php
+++ var/www/html/SignUp.php
@@ -6,7 +6,6 @@
 	$LastName = $inData["LastName"];
     $Login = $inData["Login"];
     $Password = $inData["Password"];
-
 	
 	$conn = new mysqli("localhost", "TheCourtmac", "WeLoveCOP4331", "COP4331"); 	
 	if( $conn->connect_error )
@@ -15,10 +14,9 @@
 	}
 	else
 	{	
-		$insert_val = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES (?,?,?,?)");
+		$insert_val = $conn->prepare("INSERT INTO Users (firstName, lastName, login, password) VALUES (?,?,?,?)");
 		$insert_val->bind_param("ssss", $FirstName, $LastName,  $Login, $Password);
 		$insert_val->execute();
-
 		$insert_val->close();
 		$conn->close();
 		returnWithError("");
